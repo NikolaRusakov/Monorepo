cee594ec0d78f90fd74d8c42e3061a352925659c
Nikola Rusakov <rusakov.nikola@gmail.com>
2025-09-25 20:10:02 +1000

    feat(api): generate api client from nx script

---
+++ frontend/libs/contact-service/src/lib/api/index.msw.ts
@@ -0,0 +1,4 @@
+export * from './product-module/product-module.msw'
+export * from './weather-forecast/weather-forecast.msw'
+export * from './user-module/user-module.msw'
+export * from './contacts/contacts.msw'
