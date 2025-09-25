f57ac1a396f89724746ad1250e6c4998fd99cca0
Nikola Rusakov <rusakov.nikola@gmail.com>
2025-09-25 10:16:18 +1000

    feat(AppHost): add docker svcs

--- ContactService.AppHost/AppHost.cs
+++ ContactService.AppHost/AppHost.cs
@@ -1,16 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var data = builder.AddAzurePostgresFlexibleServer("data").RunAsContainer();
+var infra = "infrastructure";
+var minio = builder.AddContainer("minio", "minio/minio:latest")
+    .WithEnvironment("MINIO_ROOT_USER", "minioadmin")
+    .WithEnvironment("MINIO_ROOT_PASSWORD", "minioadmin")
+    .WithArgs("server", "/data", "--console-address", ":9001")
+    .WithHttpEndpoint(9000, 9000, name: "api")
+    .WithHttpEndpoint(9001, 9001, name: "console");
 
-// builder.AddProject<Projects.ContactService_ApiService>()
-//     .WithReference(data);
+
+var verdaccio = builder.AddDockerfile("verdaccio", $"{infra}/verdaccio", "Dockerfile")
+    .WithEnvironment("VERDACCIO_PORT", "4873")
+    .WithHttpEndpoint(port: 4873, targetPort: 4873, name: "verdaccio")
+    .WithVolume($"{infra}/verdaccio/conf", "/verdaccio/conf'");
 
 var apiService = builder
-	.AddProject<Projects.ContactService_ApiService>("contactservice")
-	.WithReference(data);
-
-// builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
-//        .WithReference(postgresdb)
-//        .WaitForCompletion(migration);
+    .AddProject<Projects.ContactService_ApiService>("contactservice")
+    .WithHttpHealthCheck("/health")
+    .WithReference(data)
+    .WaitFor(data);
 
 builder.Build().Run();
